fcb624460bb6966d0f39d9bcf514fa2b61f7cc75
Marcos Paulo <marcos.paulo.nascimento1@gmail.com>
2026-02-19 00:00:16 -0300

    ajustes testes

--- TesteTecnicoEluminiB3.Presentation/src/app/pages/calculadora-investimento.component/calculadora-investimento.component.spec.ts
+++ TesteTecnicoEluminiB3.Presentation/src/app/pages/calculadora-investimento.component/calculadora-investimento.component.spec.ts
@@ -84,6 +84,57 @@ describe('CalculadoraInvestimentoComponent', () => {
     expect(component.carregando).toBe(false);
   });
 
+  it('deve chamar o serviço se o formulário for válido', () => {
+    component.formulario.controls.valorInicial.setValue(1000 as any);
+    component.formulario.controls.prazo.setValue(10 as any);
+
+    component.enviar();
+
+    httpMock.expectOne((request) => request.url.includes('/api/calculadora-investimento/cdb'));
+    expect(component.carregando).toBe(true);
+  });
+
+  it('deve processar o retorno da API e exibir os valores formatados no HTML', () => {
+    // 1. Arrange: Dados fictícios que sua API .NET enviaria
+    const dadosMockadoDaApi = {
+      valorBruto: 'R$ 138,140',
+      valorLiquido: 'R$ 135,11',
+      rendimento: 'R$ 15,14',
+      aliquotaAplicada: '20,00%',
+      valorImposto: 'R$ 3,03',
+      prazo: 12,
+      valorInicial: 'R$ 123,00',
+      tipoInvestimento: 'CDB'
+    };
+
+    // Preenchemos o form para permitir o envio
+    component.formulario.controls.valorInicial.setValue(1000 as any);
+    component.formulario.controls.prazo.setValue(12 as any);
+
+    // 2. Act: Dispara a ação de enviar
+    component.enviar();
+
+    // 3. Intercepta a requisição HTTP pendente
+    const req = httpMock.expectOne(request => request.url.includes('/api/calculadora-investimento/cdb'));
+
+    // Responde a requisição com o mock
+    req.flush(dadosMockadoDaApi);
+
+    // Força o Angular a atualizar o HTML com os novos dados do 'resultado'
+    fixture.detectChanges();
+
+    // 4. Assert: Verifica se o objeto foi preenchido corretamente
+    expect(component.resultado).toEqual(dadosMockadoDaApi);
+    expect(component.carregando).toBe(false);
+
+    //// Verifica se o valor aparece na tela (Supondo que você use um id ou tag para o resultado)
+    //const debugElement = fixture.nativeElement;
+    //const textoNaTela = debugElement.textContent;
+
+    //// Verifica se o valor líquido formatado como moeda aparece no HTML
+    //expect(textoNaTela).toContain('1.080,25');
+  });
+
   afterEach(() => {
     httpMock.verify(); // Garante que não há requisições pendentes
   });
